ad34327e3d18f8d766b284fb7313f33b15a3e4f4
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-12 18:33:58 -0500

    include semi colon in some lines

--- src/app/(utils)/AuthContext.jsx
+++ src/app/(utils)/AuthContext.jsx
@@ -1,17 +1,17 @@
-'use client'
+'use client';
 import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = () => {
   return useContext(AuthContext);
