2e2d7e41aef35f59649d43253173fb8df2795453
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-12 17:34:56 -0500

    fetching data from db about tournaments and detail

--- src/app/torneos/[id]/page.jsx
+++ src/app/torneos/[id]/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Loader from '@/app/(components)/Loader'
+import React, { Suspense } from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+async function getTournament(detail) {
+  const tournamentResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tournament/${detail}`)
+  return tournamentResponse.json()
 }
 
-export default page
+export default async function TournamentDetail({ params }) {
+  const tournament = await getTournament(params.id)
+  const { data } = tournament
+
+  return (
+    <div className='flex flex-col items-center justify-start mt-16'>
+      <Suspense fallback={<Loader />}>
+        <h1 className='text-blue-500 text-3xl text-center min-[450px]:text-5xl'>{data.name}</h1>
+        <p className='mt-4 text-center text-xl min-[450px]:text-3xl'>{data.description}</p>
+        <p className='mt-4 text-center text-xl min-[450px]:text-3xl'>Fecha: {data.date}</p>
+        <p className='mt-4 text-center text-xl min-[450px]:text-3xl'>Lugar: {data.location}, {data.city}</p>
+        <p className='mt-4 text-center text-xl min-[450px]:text-3xl'>Precio: ${data.price}</p>
+        <button
+          className='bg-blue-500 rounded-lg p-2 mt-4 transition duration-300 hover:bg-blue-400 min-[320px]:px-2 min-[320px]:py-1 sm:px-3 sm:py-2 sm:text-xl md:mt-6'
+        >
+          Inscibirme
+        </button>
+      </Suspense>
+    </div>
+  )
+}
--- src/app/torneos/page.jsx
+++ src/app/torneos/page.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import Loader from '../(components)/Loader';
+import Link from 'next/link';
 
-const Tournament = () => {
-  return (
-    <div>Tournamet</div>
-  )
+async function getTournaments() {
+  const tournamentResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tournament`)
+  return tournamentResponse.json()
 }
 
-export default Tournament
+export default async function Tournament() {
+  const tournaments = await getTournaments()
+
+  return (
+    <div className='flex flex-col items-center justify-start'>
+      <h2 className='text-3xl sm:text-4xl my-10'>Torneos disponibles</h2>
+      <Suspense fallback={<Loader />}>
+        <div className='w-11/12 my-0 m-auto flex flex-wrap justify-center gap-6'>
+          {tournaments.data.map(tournament => {
+            return (
+              <div
+                key={tournament._id}
+                className='border-2 border-blue-500 p-3 rounded-xl sm:w-72'
+              >
+                <h2 className='text-3xl text-blue-500'>{tournament.name}</h2>
+                <h3 className='text-xl'>Lugar: {tournament.location}, {tournament.city}</h3>
+                <p className='text-xl'>Fecha: {tournament.date}</p>
+                <Link
+                  href={`/torneos/${tournament._id}`}
+                  className='text-blue-500 text-center hover:font-bold'
+                >
+                  Ver más
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      </Suspense>
+    </div>
+  )
+}
