e0d0b0bcded39dd84c000d906b86cff5f52cc8d6
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-13 11:57:29 -0500

    change in loading option to include loader component

--- src/app/(components)/HOC/AuthenticatedRoute.js
+++ src/app/(components)/HOC/AuthenticatedRoute.js
@@ -2,42 +2,25 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
+import { useAuth } from '@/app/(utils)/AuthContext'
+import Loader from '../Loader'
 
 const authenticatedRoute = (Component = null, options = {}) => {
   const AuthenticatedRoute = (props) => {
     const router = useRouter()
-    const isLogged = Cookies.get('isLoggedIn') === 'true'
+    const { isLoggedIn } = useAuth()
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-      if (isLogged) {
+      if (isLoggedIn) {
         setLoading(false)
       } else {
         router.replace(options.pathAfterFailure || '/')
       }
-    }, [isLogged, router])
+    }, [isLoggedIn, router])
 
     if (loading) {
-      return (
-        <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-          <div className="wheel"></div>
-          <div className="hamster">
-            <div className="hamster__body">
-              <div className="hamster__head">
-                <div className="hamster__ear"></div>
-                <div className="hamster__eye"></div>
-                <div className="hamster__nose"></div>
-              </div>
-              <div className="hamster__limb hamster__limb--fr"></div>
-              <div className="hamster__limb hamster__limb--fl"></div>
-              <div className="hamster__limb hamster__limb--br"></div>
-              <div className="hamster__limb hamster__limb--bl"></div>
-              <div className="hamster__tail"></div>
-            </div>
-          </div>
-          <div className="spoke"></div>
-        </div>
-      )
+      return <Loader />
     }
 
     return <Component {...props} />
