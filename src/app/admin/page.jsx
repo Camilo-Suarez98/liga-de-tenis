a95e907f89e9bd8a08687e47ef4179f1bb1cb5fa
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-13 11:58:57 -0500

    include fetch about tournaments and users

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,35 +1,79 @@
-import React, { Suspense } from 'react'
-import Link from 'next/link'
-import Loader from '../(components)/Loader'
+/* eslint-disable @next/next/no-async-client-component */
+'use client';
+import React, { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
+import Loader from '../(components)/Loader';
 
 async function getUsers() {
-  const usersResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/user`)
-  return usersResponse.json()
-}
+  const usersResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/user`);
+  return usersResponse.json();
+};
+
+async function getTournaments() {
+  const tournamentResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tournament`);
+  return tournamentResponse.json();
+};
 
 export default async function Dashboard() {
+  const router = useRouter();
+  const users = await getUsers();
+  const tournaments = await getTournaments();
 
-  const res = await getUsers()
-  console.log(res.data);
+  const handleCreateTournament = () => {
+    router.push('/torneos/nuevo-torneo')
+  };
 
   return (
-    <div>
-      <h2>Lista de Torneos</h2>
-      {/* <Link
-        href='/torneos/nuevo-torneo'
-        className='bg-blue-500  rounded-lg p-2 transition duration-300 hover:bg-transparent hover:border-2 hover:border-blue-500 min-[320px]:px-2 min-[320px]:py-1 sm:px-3 sm:py-2 sm:text-xl md:mt-6'
-      >
-        Nuevo Torneo
-      </Link> */}
-      {/* table here */}
+    <div className='flex flex-col items-center justify-start mt-10'>
+      <div className='flex justify-around items-center sm:w-full'>
+        <h2 className='text-4xl my-4 sm:text-5xl'>Lista de Torneos</h2>
+        <button
+          onClick={handleCreateTournament}
+          className='bg-blue-500 rounded-lg p-2 transition duration-300 hover:bg-transparent hover:border-2 hover:border-blue-500 min-[320px]:px-2 min-[320px]:py-1 sm:px-3 sm:py-2 sm:text-xl'
+        >
+          Nuevo Torneo
+        </button>
+      </div>
+      <Suspense fallback={<Loader />}>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <span>Nombre</span>
+              </th>
+              <th>
+                <span>Ciudad</span>
+              </th>
+              <th>
+                <span>Fecha</span>
+              </th>
+              <th>
+                <span>Participantes</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tournaments.data.map(tournament => {
+              return (
+                <tr key={tournament._id}>
+                  <td>{tournament.name}</td>
+                  <td>{tournament.city}</td>
+                  <td>{tournament.city}</td>
+                  <td>{tournament.participants.length === 0 ? 'No hay participantes' : tournament.participants}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </Suspense>
       <h2>Lista de usuarios</h2>
       <Suspense fallback={<Loader />}>
-        {res.data.map(user => {
+        {users.data.map(user => {
           return (
             <div key={user._id}>{user.name}</div>
           )
         })}
       </Suspense>
     </div>
-  )
-}
+  );
+};
