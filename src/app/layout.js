0a9aa860a025245ba6467309feea37cbf89b9f9e
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-14 22:56:14 -0500

    configure third party authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Header from '../components/Header';
 import { AuthProvider } from '../utils/AuthContext';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'Liga de Tenis NJS',
@@ -11,10 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='p-2'>
-        <AuthProvider>
-          <Header />
-          {children}
-        </AuthProvider>
+        <Providers>
+          <AuthProvider>
+            <Header />
+            {children}
+          </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
